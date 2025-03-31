aa73ab7d835fb4f4c307420411a031a16f2fafbb
mora-suekko <manarbekov2006@gmail.com>
2025-04-01 02:38:16 +0500

    проверка входа

--- app/src/main/java/com/example/lab7/LoginActivity.kt
+++ app/src/main/java/com/example/lab7/LoginActivity.kt
@@ -1,17 +1,54 @@
 package com.example.lab7
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+
+        if (sharedPref.getBoolean("isLoggedIn", false)) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            return
+        }
+
         setContentView(R.layout.activity_login)
 
+        val edUsername = findViewById<EditText>(R.id.ed_username)
+        val edPassword = findViewById<EditText>(R.id.ed_password)
+        val btnLogin = findViewById<Button>(R.id.btn_login)
         val btnSignUp = findViewById<Button>(R.id.btn_signup)
+
+        btnLogin.setOnClickListener {
+            val enteredUsername = edUsername.text.toString()
+            val enteredPassword = edPassword.text.toString()
+
+            val savedUsername = sharedPref.getString("Username", null)
+            val savedPassword = sharedPref.getString("Password", null)
+
+            if (enteredUsername == savedUsername && enteredPassword == savedPassword) {
+                Toast.makeText(this, "Вход выполнен!", Toast.LENGTH_SHORT).show()
+
+                val editor = sharedPref.edit()
+                editor.putBoolean("isLoggedIn", true)
+                editor.apply()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Неверный логин или пароль", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
--- app/src/main/java/com/example/lab7/MainActivity.kt
+++ app/src/main/java/com/example/lab7/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.lab7
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLogout = findViewById<Button>(R.id.btn_logout)
+
+        val sharedPref = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+
+        btnLogout.setOnClickListener {
+            val editor = sharedPref.edit()
+            editor.remove("isLoggedIn")
+            editor.apply()
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
