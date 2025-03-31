1daa94a0717c2df9b839904a345a9859531ccd9b
mora-suekko <manarbekov2006@gmail.com>
2025-04-01 02:18:22 +0500

    кнопка регистраций кнопка назад

--- app/src/main/java/com/example/lab7/LoginActivity.kt
+++ app/src/main/java/com/example/lab7/LoginActivity.kt
@@ -1,11 +1,20 @@
-package com.example.lab7
+package com.example.myapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnSignUp = findViewById<Button>(R.id.btn_signup)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
